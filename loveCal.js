b25fc6beae8649c231df22d5ea11725aab1071a5
djena <114151571+Dibyajyoti-08@users.noreply.github.com>
2024-02-15 10:16:21 +0530

    Add files via upload

---
+++ loveCal.js
@@ -0,0 +1,4 @@
+prompt("What is the 1st person name : ");
+prompt("What is the 2nd person name : ");
+var loveCal = Math.floor(Math.random() * 100) + 1;
+alert("Your love percentile is " + loveCal + "%");
